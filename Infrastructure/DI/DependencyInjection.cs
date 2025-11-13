d68c977d4e4cfdebec5b8f6d5844e0caf2dd2ea7
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-11-13 20:24:22 +0200

    register the bachground service for DI

--- Infrastructure/DI/DependencyInjection.cs
+++ Infrastructure/DI/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Repos;
 using Infrastructure.Repos;
+using Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 namespace Infrastructure.DI;
 
@@ -8,6 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddSingleton<ICountryRepo, CountryRepo>();
+        services.AddHostedService<TemporalBlockCleanupService>();
         // register other infra services here
         return services;
     }
